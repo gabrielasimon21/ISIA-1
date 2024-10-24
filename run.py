8941c6c28ace6f2a94c69cf70e9603de42e3d7bb
franciscovmacieira <francisco.v.macieira@gmail.com>
2024-10-24 11:20:28 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,44 +1,42 @@
-import spade
-import tkinter as tk
-import random
-from spade.agent import Agent
-from spade.behaviour import CyclicBehaviour
-from spade.template import Template
-from spade.message import Message
 import asyncio
-from map import Map
+import logging
 from civil_agent import Civil
+from ShelterAgent import ShelterAgent
+from map import Map
+
+logging.basicConfig(level=logging.DEBUG)
 
 async def main():
-    # Create and initialize the environment
-    environment = Map()
-    environment.create_gui()
-    civil_agents = {}
-    k = 1
-    for i in range (20):
-        for j in range (20):
-            population = environment.population_data [i][j]
-            civil_agents[k] = Civil(f"civil{k}@localhost", "password", environment,[i, j], True, False, False)
-            print (f"Agente{k}")
-            n_civis_criados = 1
-            k += 1
-            for n in range (environment.informations[(i, j)] [0]):
-                civil_agents[k] = Civil(f"civil{k}@localhost", "password", environment, [i, j], False, True, False)
-                n_civis_criados += 1
+    try:
+        map = Map()
+        map.create_gui()
+
+        civil_agents = {}
+        k = 1
+        for i in range(20):
+            for j in range(20):
+                civil_agents[k] = Civil(f"civil{k}@localhost", "password", map, [i, j], True, False, False)
+                n_civis_criados = 1
                 k += 1
-                print(f"Agente{k}")
-            for n in range (environment.informations[(i, j)] [0]):
-                civil_agents[k] = Civil(f"civil{k}@localhost", "password", environment, [i, j], False, False, True)
-                n_civis_criados += 1
-                k += 1
-                print(f"Agente{k}")
-            while (n_civis_criados < population):
-                civil_agents[k] = Civil(f"civil{k}@localhost", "password", environment, [i, j], False, False, False)
-                n_civis_criados += 1
-                k += 1
-                print(f"Agente{k}")
-    for civil_agent in civil_agents.values():
-        await civil_agent.start(auto_register=True)
+                for n in range(map.informations[(i, j)][0]):
+                    civil_agents[k] = Civil(f"civil{k}@localhost", "password", map, [i, j], False, True, False)
+                    n_civis_criados += 1
+                    k += 1
+                for n in range(map.informations[(i, j)][1]):
+                    civil_agents[k] = Civil(f"civil{k}@localhost", "password", map, [i, j], False, False, True)
+                    n_civis_criados += 1
+                    k += 1
+
+        for civil_agent in civil_agents.values():
+            await civil_agent.start(auto_register=True)
+            if civil_agent.leader:
+                print("Sender started")
+
+        shelter_agent = ShelterAgent("shelter1@localhost", "password", [0, 0], 10, map)
+        await shelter_agent.start(auto_register=True)
+        print("Receiver started")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    spade.run(main())
+    asyncio.run(main())
