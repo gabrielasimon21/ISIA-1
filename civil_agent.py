b74063a79c793ca66d381b19a9dc3dd879d6e611
franciscovmacieira <francisco.v.macieira@gmail.com>
2024-10-24 11:20:05 +0100

    Add files via upload

--- civil_agent.py
+++ civil_agent.py
@@ -2,7 +2,7 @@ import spade
 import tkinter as tk
 import random
 from spade.agent import Agent
-from spade.behaviour import CyclicBehaviour
+from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade.template import Template
 from spade.message import Message
 import asyncio
@@ -16,16 +16,30 @@ class Civil(Agent):
         self.dead = dead
         self.hurt = hurt
 
-        async def setup(self):
-            class CivilInteraction(CyclicBehaviour):
-                async def run(self):
-                    print("Agente iniciado")
-                    self.affected = environment.affected_points(position)
-                    self.n_mortos = environment.n_mortos()
-                    self.n_feridos = environment.n_feridos()
+    async def setup(self):
+        if self.leader:
+            print("Agente iniciado")
+            print("SenderAgent started")
+            b = self.InformBehav()
+            self.add_behaviour(b)
+            self.n_mortos = self.environment.get_n_mortos(self.position[0], self.position[1])  # Corrija para chamar o método corretamente
+            self.n_feridos = self.environment.get_n_feridos(self.position[0], self.position[1])  # Corrija para chamar o método corretamente
 
-                    def communicate (self):
-                        if self.affected == True and self.leader == True:
-                            print (f"Nº de mortos: {self.n_mortos}, Nº de feridos: {self.n_feridos}")
-            self.communicate()
-            self.add_behaviour(CivilInteraction())
+    def communicate(self):
+        if self.leader:
+            print(f"Nº de mortos: {self.n_mortos}, Nº de feridos: {self.n_feridos}")
+
+    class InformBehav(OneShotBehaviour):
+        async def run(self):
+            msg = Message(to="shelter1@localhost")  # Instantiate the message
+            msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
+            msg.set_metadata("ontology", "myOntology")  # Set the ontology of the message content
+            msg.set_metadata("language", "OWL-S")  # Set the language of the message content
+            msg.body = "Hello World"  # Set the message content
+            await self.send(msg)
+            print("Message sent!")
+            self.exit_code = "Job Finished!"  # Set exit_code for the behaviour
+            await self.agent.stop()  # Stop agent from behaviour
+
+
+
